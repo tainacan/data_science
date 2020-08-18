bfab1f3261e8c2c78b2ef75e4db685b7b46fde9d
luisfeliperd <luisfelipeprf@gmail.com>
2020-08-18 11:19:00 -0300

    Update webScraping_Iphan_SicgInstituicao.py

--- FAPESP/coleta_amostral/webScraping_Iphan_SicgInstituicao.py
+++ FAPESP/coleta_amostral/webScraping_Iphan_SicgInstituicao.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 16 18:43:20 2020
-
-@author: luisr
-"""
 import pandas as pd
 import time
 import random
 resultado = pd.DataFrame()
 notFind = []
 
-#%%
 for i in range(101):
     n = random.randint(1,450)
     time.sleep(2)
@@ -45,5 +38,4 @@ for i in range(101):
                 
     resultado = resultado.append(resultDict, ignore_index=True)
 
-#%%
 resultado.to_csv("iphan_SICG_instituicao.csv", index=False)
