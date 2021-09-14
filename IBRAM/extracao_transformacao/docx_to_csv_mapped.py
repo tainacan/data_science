d8a9a8aa049997d8ba0c1242c33d878ec6278e50
luisfeliperd <luisfelipeprf@gmail.com>
2021-09-14 16:39:57 -0300

    Update docx_to_csv_mapped.py

--- IBRAM/extracao_transformacao/docx_to_csv_mapped.py
+++ IBRAM/extracao_transformacao/docx_to_csv_mapped.py
@@ -1,104 +1,46 @@
-from docx.api import Document
-from collections import defaultdict
-import win32com.client as win32
-from unidecode import unidecode
-from time import sleep #define um periodo de tempo entre uma ação e outra
-from tqdm import tqdm #adiciona uma barra de "loading"
-import glob
+import glob #Lista as pastas e arquivos
+from docx.api import Document #Manipulação de Arquivos docx
+import win32com.client as win32 #Manipulação de Arquivos docx
 import csv
+import time
 import os
 import pandas as pd
 
-#Mapear os metadados para extração dos dados do texto das fichas
-metadata_dict = {"Report":"","Familia":"","Especie":"","Procedencia":"","Codigo":"","Registro no":"","Camadas de Crescimento":"",
-                 "Vasos":"","Observacoes":"","Parenquima axial":"","Estrutura estratificada":"","Raios":"",
-                 "Traqueides e fibras":"","Inclusoes minerais":"","Referencias bibliograficas":"","Dados ecologicos":"",
-                 "Gaveta":"","Procedencia":"","Limites":"",
-                 "Porosidade":"","Arranjo":"","Agrupamento":"","Diametro tangencial medio":"",
-                 "Numero medio de vasos por mm2":"","Tiloses":"","Observacoes":"","Placas de perfuracao":"",
-                 "Pontoacoes intervasculares":"","Pontoacoes guarnecidas":"","Pontoacoes raio-vasculares":"",
-                 "Parenquima paratraqueal":"","Seriacao":"","Numero medio de raios por mm":"","Tamanho":"",
-                 "Caracteristicas especiais":"","Composicao celular":"","Floema incluso":"",
-                 "Cristais prismaticos":"","Outros cristais":"","Caracteristicas diagnosticas":"",
-                 "Fibras":"","Pontoacoes das fibras":"","Espessura da parede das fibras":"",
-                 "Canais intercelulares":""}
+#Aciona a api do microsoft word
+word = win32.gencache.EnsureDispatch('Word.Application')
+word.Visible = 0
 
-#O script coleta o texto de cada documento docx e separa os termos do texto pelo espaço, colocando os termos em uma lista.
+files = glob.glob("/Fichas/*.docx") #lista os arquivos com a extensão .docx
 
-#Os metadados mapeados são buscados pelos índices da lista de termos obtida. O valor dos metadados é obtido ao delimitar o 
-#conjunto de termos entre um metadado encontrado e o próximo.
 
-result_dict = defaultdict(list)
-
-for file in glob.glob(''):
+#Grava os dados de cada tabela da ficha em células em uma planilha CSV
+with open("_v2.csv", "w", encoding='utf-8', newline='') as f:
+    f_writer = csv.writer(f)
     
-    word = win32.Dispatch('Word.Application')
-    word.Visible = 0
-    
-    document = Document(file)
-    doc = word.Documents.Open(file)
-    
-    print("r"+ file.split("\\")[-1].strip(".docx").strip(".rtf"), end=",")
-    
-    paragraph_list = []
-    for paragraph in doc.paragraphs:
-
-        sentence = str(paragraph).rstrip('\r\x07').replace("\t\t"," ").strip("\t")
-        words = sentence.split(":")
-
-        for word in words:
-
-            word=unidecode(word)
-
-            if "\t\x0b" in word:
-                unicode_words = word.splitlines()
-
-                for unicode_word in unicode_words:
-
-                    if unicode_word != '':
-                        paragraph_list.append(unicode_word.strip())
-
-            elif "\x0b" in word:
-                unicode_words = word.splitlines()
-
-                for unicode_word in unicode_words:
-
-                    if unicode_word != '':
-                        paragraph_list.append(unicode_word.strip())
+    for ficha in files:
+            print("*****************************")
+            print("Acessando a Ficha ", ficha)
+            
+            if "~$" in ficha: #Pula arquivos temporários do Word
+                continue
+                print("Pulando Arquivo Temporário")
+                
             else:
-                if word != '':
-                    paragraph_list.append(word.strip())
-    
-    values_dict = defaultdict(list)
-
-    for key in metadata_dict.keys():
-
-        for value_i1 in paragraph_list:
-
-            if unidecode(value_i1) == key:
-
-                i_1 = paragraph_list.index(value_i1)
-                key_1 = key
-
-                for value_i2 in paragraph_list[i_1+1:]:
-
-                    if value_i2 in metadata_dict.keys():
-
-                        i_2 = paragraph_list[i_1:].index(value_i2)
-                        break
-
-                values_dict[key_1].append(" ".join(paragraph_list[i_1+1:i_1+i_2]))
-    
-    values_dict['Report'].append("r"+ file.split("\\")[-1].strip(".docx").strip(".rtf"))
-    
-    for metadata in metadata_dict.keys():
-
-        if metadata in values_dict.keys():
-            result_dict[metadata].append("||".join(list(set(values_dict[metadata]))))
-        else:
-            result_dict[metadata].append("")
-    doc.Close()
-    sleep(2.5)
-    
-result_df = pd.DataFrame.from_dict(result_dict)
-result_df.to_csv('', index=False)
+                doc = word.Documents.Open(ficha) #Abre a ficha
+                
+                tables = doc.Tables #Encontra as tabelas da ficha
+                
+                values_list = []#Lista para armazenar os dados das tabelas encontradas
+                values_list.append(ficha.split("/")[-1]) 
+                
+                for table in tables: #Looping para percorrer a matriz de dados das tabelas (Colunas e Linhas)
+                    for column in range(len(table.Columns)):
+                        for row in range(len(table.Rows)):
+                            try:
+                                values_list.append(table.Cell(Row = row+1, Column = column+1).Range.Text.rstrip('\r\x07'))#Adiciona os dados de cada tabela encontrada em uma lista
+                            except:
+                                #Exceção criada pois a numeração de identação da tabela ultrapassa o limite na busca após a última tabela
+                                continue
+                doc.Close(False)#Fecha a ficha em Word
+                
+                f_writer.writerow(values_list)#Salva uma linha de dados no CSV
